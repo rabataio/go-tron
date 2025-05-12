5ce5bd46ee3879f04b2b2d906ca9258142c09eee
dyus <dyuuus@gmail.com>
2025-05-13 01:06:52 +0300

    add rateLimitInterceptor

--- client/client.go
+++ client/client.go
@@ -6,9 +6,15 @@ import (
 
 	timeoutmiddleware "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/timeout"
 	pbapi "github.com/rabataio/go-tron/proto/api"
+	"golang.org/x/time/rate"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
+)
+
+const (
+	defaultRateLimit = 15
+	defaultBurst     = 1
 )
 
 func tokenInterceptor(token string) grpc.UnaryClientInterceptor {
@@ -26,11 +32,31 @@ func tokenInterceptor(token string) grpc.UnaryClientInterceptor {
 	}
 }
 
+func rateLimitInterceptor(limiter *rate.Limiter) grpc.UnaryClientInterceptor {
+	return func(
+		ctx context.Context,
+		method string,
+		req, reply any,
+		cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
+		if err := limiter.Wait(ctx); err != nil {
+			return err
+		}
+
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+}
+
 func NewConnection(endpoint string, token string, timeout time.Duration) (*grpc.ClientConn, error) {
+	limiter := rate.NewLimiter(rate.Limit(defaultRateLimit), defaultBurst)
+
 	connection, err := grpc.NewClient(
 		endpoint,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithChainUnaryInterceptor(
+			rateLimitInterceptor(limiter),
 			tokenInterceptor(token),
 			timeoutmiddleware.UnaryClientInterceptor(timeout),
 		),
